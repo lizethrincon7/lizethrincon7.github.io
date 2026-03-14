6736cf810e8b23aae3b60836f3ab81950e047f18
Lizeth Rincon <rincon.lizethisc@gmail.com>
2026-03-13 20:44:28 -0600

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: "https://lizethrincon7.github.io",
   integrations: [tailwind(), react()],
 });
